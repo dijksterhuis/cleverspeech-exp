4c385ffc75ea2af63ad837aaef845e116d569418
dijksterhuis <mrobeson@dundee.ac.uk>
2021-01-25 18:12:00 +0000

    REFACTOR: Rename the experiment to something more accurate -- no longer necessarily looking for high confidnece, more analysing the effects of two edge cases alignments.

---
+++ CTCAlignments/attacks.py
@@ -0,0 +1,389 @@
+#!/usr/bin/env python3
+import os
+
+from cleverspeech.graph.GraphConstructor import Constructor
+from cleverspeech.graph import Constraints
+from cleverspeech.graph import Graphs
+from cleverspeech.graph import Optimisers
+from cleverspeech.graph import Procedures
+from cleverspeech.graph import Outputs
+from cleverspeech.data import Feeds
+from cleverspeech.data import ETL
+from cleverspeech.data import Generators
+from cleverspeech.utils.Utils import log, args
+
+from SecEval import VictimAPI as DeepSpeech
+
+
+from boilerplate import execute
+
+# local attack classes
+import custom_defs
+import custom_etl
+
+GPU_DEVICE = 0
+MAX_PROCESSES = 3
+SPAWN_DELAY = 60 * 5
+
+TOKENS = " abcdefghijklmnopqrstuvwxyz'-"
+BEAM_WIDTH = 500
+
+AUDIOS_INDIR = "./samples/all/"
+TARGETS_PATH = "./samples/cv-valid-test.csv"
+OUTDIR = "./adv/ctc-alignments/"
+
+# targets search parameters
+MAX_EXAMPLES = 1000
+MAX_TARGETS = 1000
+MAX_AUDIO_LENGTH = 120000
+
+RESCALE = 0.95
+CONSTRAINT_UPDATE = "geom"
+LEARNING_RATE = 10
+NUMB_STEPS = 10000
+DECODING_STEP = 10
+BATCH_SIZE = 10
+
+N_RUNS = 1
+
+
+def get_dense_batch_factory(settings):
+
+    # get N samples of all the data. alsp make sure to limit example length,
+    # otherwise we'd have to do adaptive batch sizes.
+
+    audio_etl = ETL.AllAudioFilePaths(
+        settings["audio_indir"],
+        settings["max_examples"],
+        filter_term=".wav",
+        max_samples=settings["max_audio_length"]
+    )
+
+    all_audio_file_paths = audio_etl.extract().transform().load()
+
+    targets_etl = ETL.AllTargetPhrases(
+        settings["targets_path"], settings["max_targets"],
+    )
+    all_targets = targets_etl.extract().transform().load()
+
+    # Generate the batches in turn, rather than all in one go ...
+
+    batch_factory = custom_etl.CTCHiScoresBatchGenerator(
+        all_audio_file_paths, all_targets, settings["batch_size"]
+    )
+
+    # ... To save resources by only running the final ETLs on a batch of data
+
+    batch_gen = batch_factory.generate(
+        ETL.AudioExamples, custom_etl.RepeatsTargetPhrases, Feeds.Attack
+    )
+
+    log(
+        "New Run",
+        "Number of test examples: {}".format(batch_factory.numb_examples),
+        ''.join(["{k}: {v}\n".format(k=k, v=v) for k, v in settings.items()]),
+    )
+    return batch_gen
+
+
+def ctc_dense_alignment_run(master_settings):
+    """
+    Repeating characters (linear expansion) from a target transcription to
+    create an alignment doesn't help the adversary. There's a 0.5 drop in
+    cumulative log probabilities and a tripling of the required modification
+    size (relative to maximum possible size).
+
+    We want to maximise the classifier confidence for our attacks (see Wild
+    Patterns -- Biggio et al. about the perturbation size misconception).
+
+    CTC Loss is used to perform maximum likelihood optimisation. CTC
+    Loss is usually used with respect to a target transcription (where the
+    length of the target transcription is less than or equal to the number of
+    frames). But we can trick CTC Loss into optimising an alignment with two key
+    changes.
+
+    The below settings are an edge case for CTC Loss use.
+    ```
+        preprocess_collapse_repeated=False,
+        ctc_merge_repeated=True,
+    ```
+
+    Enabling them means that:
+    (a) duplicated characters in a target sequence are not collapsed during
+    pre-processing
+    (b) duplicated characters are not merged during optimisation.
+
+    But we can't pass in an alignment sequence as a target without tricking CTC
+    Loss into treating the blank token `-` as a real/valid character.
+
+    The network's softmax matrix output is extended with an M (n_frames) length
+    vector of zero values. These extra values act as a "dummy" blank token,
+    which will never be likely.
+
+    Now we can include the blank `-` token in the target sequence.
+
+    This obviously modifies one of the conditions for CTC Loss -- now the target
+    sequence length must be equal to the number of audio frames.
+
+    :return: None
+    """
+    def create_attack_graph(sess, batch, settings):
+
+        attack = Constructor(sess, batch)
+
+        attack.add_hard_constraint(
+            Constraints.L2,
+            r_constant=settings["rescale"],
+            update_method=settings["constraint_update"],
+        )
+
+        attack.add_graph(
+            Graphs.SimpleAttack
+        )
+
+        attack.add_victim(
+            DeepSpeech.Model,
+            tokens=settings["tokens"],
+            beam_width=settings["beam_width"]
+        )
+
+        attack.add_adversarial_loss(custom_defs.RepeatsCTCLoss)
+
+        attack.create_loss_fn()
+
+        attack.add_optimiser(
+            Optimisers.AdamOptimiser,
+            learning_rate=settings["learning_rate"]
+        )
+
+        attack.add_procedure(
+            Procedures.UpdateBound,
+            steps=settings["nsteps"],
+            decode_step=settings["decode_step"]
+        )
+
+        attack.add_outputs(
+            Outputs.Base,
+            settings["outdir"],
+        )
+
+        return attack
+
+    for run in range(0, N_RUNS):
+
+        outdir = os.path.join(OUTDIR, "dense/")
+        outdir = os.path.join(outdir, "run_{}/".format(run))
+
+        settings = {
+            "audio_indir": AUDIOS_INDIR,
+            "targets_path": TARGETS_PATH,
+            "outdir": outdir,
+            "batch_size": BATCH_SIZE,
+            "tokens": TOKENS,
+            "nsteps": NUMB_STEPS,
+            "decode_step": DECODING_STEP,
+            "beam_width": BEAM_WIDTH,
+            "constraint_update": CONSTRAINT_UPDATE,
+            "rescale": RESCALE,
+            "learning_rate": LEARNING_RATE,
+            "gpu_device": GPU_DEVICE,
+            "max_spawns": MAX_PROCESSES,
+            "spawn_delay": SPAWN_DELAY,
+            "max_examples": MAX_EXAMPLES,
+            "max_targets": MAX_TARGETS,
+            "max_audio_length": MAX_AUDIO_LENGTH,
+        }
+
+        settings.update(master_settings)
+
+        batch_factory = get_dense_batch_factory(settings)
+
+        execute(settings, create_attack_graph, batch_factory)
+
+        log("Finished run {}.".format(run))
+
+
+def get_sparse_batch_factory(settings):
+
+    # get N samples of all the data. alsp make sure to limit example length,
+    # otherwise we'd have to do adaptive batch sizes.
+
+    audio_etl = ETL.AllAudioFilePaths(
+        settings["audio_indir"],
+        settings["max_examples"],
+        filter_term=".wav",
+        max_samples=settings["max_audio_length"]
+    )
+
+    all_audio_file_paths = audio_etl.extract().transform().load()
+
+    targets_etl = ETL.AllTargetPhrases(
+        settings["targets_path"], settings["max_targets"],
+    )
+    all_targets = targets_etl.extract().transform().load()
+
+    # Generate the batches in turn, rather than all in one go ...
+
+    batch_factory = Generators.BatchGenerator(
+        all_audio_file_paths, all_targets, settings["batch_size"]
+    )
+
+    # ... To save resources by only running the final ETLs on a batch of data
+
+    batch_gen = batch_factory.generate(
+        ETL.AudioExamples, ETL.TargetPhrases, Feeds.Attack
+    )
+
+    log(
+        "New Run",
+        "Number of test examples: {}".format(batch_factory.numb_examples),
+        ''.join(["{k}: {v}\n".format(k=k, v=v) for k, v in settings.items()]),
+    )
+    return batch_gen
+
+
+def ctc_sparse_alignment_run(master_settings):
+    """
+    A confident sparse alignment can be derived from the argmax of a dummy
+    softmax matrix (initially zero valued) by solving the following optimisation
+    goal:
+
+        minimise_{Z} CTC Loss(Z, t')
+
+    By optimising a dummy matrix, we end up with a matrix where the most likely
+    characters per frame are (usually) the targets we want.
+
+    *Most importantly*, this optimisation is independent of any audio example --
+    the alignment we find will be the most likely alignment for _this_ model and
+    for _this_ target transcription.
+
+    Again, we want to maximise the classifier confidence for our attacks
+    (see Wild Patterns -- Biggio et al. about the perturbation size
+    misconception).
+
+    CTC Loss is used to perform maximum likelihood optimisation. CTC
+    Loss is usually used with respect to a target transcription (where the
+    length of the target transcription is less than or equal to the number of
+    frames). But we can trick CTC Loss into optimising an alignment with two key
+    changes.
+
+    The below settings are an edge case for CTC Loss use.
+    ```
+        preprocess_collapse_repeated=False,
+        ctc_merge_repeated=True,
+    ```
+
+    Enabling them means that:
+    (a) duplicated characters in a target sequence are not collapsed during
+    pre-processing
+    (b) duplicated characters are not merged during optimisation.
+
+    But we can't pass in an alignment sequence as a target without tricking CTC
+    Loss into treating the blank token `-` as a real/valid character.
+
+    The network's softmax matrix output is extended with an M (n_frames) length
+    vector of zero values. These extra values act as a "dummy" blank token,
+    which will never be likely.
+
+    Now we can include the blank `-` token in the target sequence.
+
+    This obviously modifies one of the conditions for CTC Loss -- now the target
+    sequence length must be equal to the number of audio frames.
+
+    :return: None
+    """
+    def create_attack_graph(sess, batch, settings):
+
+        attack = Constructor(sess, batch)
+
+        attack.add_hard_constraint(
+            Constraints.L2,
+            r_constant=settings["rescale"],
+            update_method=settings["constraint_update"],
+        )
+
+        attack.add_graph(
+            Graphs.SimpleAttack
+        )
+
+        attack.add_victim(
+            DeepSpeech.Model,
+            tokens=settings["tokens"],
+            beam_width=settings["beam_width"]
+        )
+
+        alignment = Constructor(attack.sess, batch)
+        alignment.add_graph(custom_defs.CTCSearchGraph, attack)
+        alignment.add_adversarial_loss(custom_defs.AlignmentLoss)
+        alignment.create_loss_fn()
+        alignment.add_optimiser(custom_defs.CTCAlignmentOptimiser)
+
+        attack.add_adversarial_loss(
+            custom_defs.RepeatsCTCLoss,
+            alignment=alignment.graph.target_alignments,
+        )
+
+        attack.create_loss_fn()
+
+        attack.add_optimiser(
+            Optimisers.AdamOptimiser,
+            learning_rate=settings["learning_rate"]
+        )
+
+        attack.add_procedure(
+            custom_defs.CTCAlignmentsUpdateHard,
+            alignment_graph=alignment,
+            steps=settings["nsteps"],
+            decode_step=settings["decode_step"],
+        )
+
+        attack.add_outputs(
+            Outputs.Base,
+            settings["outdir"],
+        )
+
+        return attack
+
+    for run in range(0, N_RUNS):
+
+        outdir = os.path.join(OUTDIR, "sparse/")
+        outdir = os.path.join(outdir, "run_{}/".format(run))
+
+        settings = {
+            "audio_indir": AUDIOS_INDIR,
+            "targets_path": TARGETS_PATH,
+            "outdir": outdir,
+            "batch_size": BATCH_SIZE,
+            "tokens": TOKENS,
+            "nsteps": NUMB_STEPS,
+            "decode_step": DECODING_STEP,
+            "beam_width": BEAM_WIDTH,
+            "constraint_update": CONSTRAINT_UPDATE,
+            "rescale": RESCALE,
+            "learning_rate": LEARNING_RATE,
+            "gpu_device": GPU_DEVICE,
+            "max_spawns": MAX_PROCESSES,
+            "spawn_delay": SPAWN_DELAY,
+            "max_examples": MAX_EXAMPLES,
+            "max_targets": MAX_TARGETS,
+            "max_audio_length": MAX_AUDIO_LENGTH,
+        }
+
+        settings.update(master_settings)
+
+        batch_factory = get_sparse_batch_factory(settings)
+
+        execute(settings, create_attack_graph, batch_factory)
+
+        log("Finished run {}.".format(run))
+
+
+if __name__ == '__main__':
+
+    experiments = {
+        "dense": ctc_dense_alignment_run,
+        "sparse": ctc_sparse_alignment_run,
+    }
+
+    args(experiments)
+
---
+++ CTCAlignments/custom_defs.py
@@ -0,0 +1,255 @@
+import tensorflow as tf
+import numpy as np
+
+from cleverspeech.graph.Procedures import Base
+from cleverspeech.utils.Utils import lcomp
+
+
+class RepeatsCTCLoss(object):
+    """
+    Adversarial CTC Loss that ignores the blank tokens for higher confidence.
+    Does not actually work for adversarial attacks as characters from target
+    transcription end up being placed in like `----o--o-oo-o----p- ...` which
+    merges down to `ooop ...`
+
+    Only used to demonstrate that regular CTC loss can't do what we want without
+    further modification to the back-end (recursive) calculations.
+
+    N.B. This loss does *not* conform to l(x + d, t) <= 0 <==> C(x + d) = t
+    """
+    def __init__(self, attack_graph, alignment=None, loss_weight=1.0):
+
+        if alignment is not None:
+            print("\n\nUsing CTC alignment\n\n")
+            self.ctc_target = tf.keras.backend.ctc_label_dense_to_sparse(
+                alignment,
+                attack_graph.batch.audios.feature_lengths,
+            )
+        else:
+            self.ctc_target = tf.keras.backend.ctc_label_dense_to_sparse(
+                attack_graph.graph.placeholders.targets,
+                attack_graph.graph.placeholders.target_lengths,
+            )
+
+        blank_pad = tf.zeros(
+            [
+                attack_graph.batch.audios.feature_lengths[0] + 1,
+                attack_graph.batch.size,
+                1
+            ],
+            tf.float32
+        )
+
+        logits_mod = tf.concat(
+            [attack_graph.victim.raw_logits, blank_pad],
+            axis=2
+        )
+
+        self.loss_fn = tf.nn.ctc_loss(
+            labels=tf.cast(self.ctc_target, tf.int32),
+            inputs=logits_mod,
+            sequence_length=attack_graph.batch.audios.feature_lengths,
+            preprocess_collapse_repeated=False,
+            ctc_merge_repeated=False,
+        ) * loss_weight
+
+
+class AlignmentLoss(object):
+    def __init__(self, alignment_graph):
+        feat_lens = alignment_graph.batch.audios.feature_lengths
+
+        self.ctc_target = tf.keras.backend.ctc_label_dense_to_sparse(
+            alignment_graph.graph.targets,
+            alignment_graph.graph.target_lengths
+        )
+
+        self.loss_fn = tf.nn.ctc_loss(
+            labels=self.ctc_target,
+            inputs=alignment_graph.graph.raw_alignments,
+            sequence_length=feat_lens
+        )
+
+
+class CTCSearchGraph:
+    def __init__(self, sess, batch, attack_graph):
+        alignment_shape = attack_graph.victim.raw_logits.shape.as_list()
+
+        self.raw_alignments = tf.Variable(
+            tf.ones(alignment_shape) * 20,
+            dtype=tf.float32,
+            trainable=True,
+            name='qq_alignment'
+        )
+
+        # TODO: apply mask
+
+        print(alignment_shape)
+
+        self.logits_alignments = tf.transpose(self.raw_alignments, [1, 0, 2])
+        self.softmax_alignments = tf.nn.softmax(self.logits_alignments)
+        self.target_alignments = tf.argmax(self.softmax_alignments, axis=2)
+
+        # TODO - this should be loaded from feeds later on
+        self.targets = attack_graph.graph.placeholders.targets
+        self.target_lengths = attack_graph.graph.placeholders.target_lengths
+
+        #super().__init__(attack_graph.sess, batch)
+
+
+class CTCAlignmentOptimiser:
+    def __init__(self, graph):
+
+        self.graph = graph
+        self.loss = self.graph.adversarial_loss
+
+        self.train_alignment = None
+
+    def create_optimiser(self):
+
+        optimizer = tf.train.AdamOptimizer(10)
+
+        grad_var = optimizer.compute_gradients(
+            self.graph.loss_fn,
+            self.graph.graph.raw_alignments
+        )
+        assert None not in lcomp(grad_var, i=0)
+
+        self.train_alignment = optimizer.apply_gradients(grad_var)
+
+    def optimise(self, batch, victim):
+
+        g, v, b = self.graph, victim, batch
+
+        logits = v.get_logits(v.raw_logits, b.feeds.examples)
+        assert logits.shape == g.graph.raw_alignments.shape
+        #g.sess.run(g.raw_alignments.assign(logits))
+
+        while True:
+
+            train_ops = [
+                    self.loss.loss_fn,
+                    g.graph.softmax_alignments,
+                    g.graph.logits_alignments,
+                    g.graph.target_alignments,
+                    self.train_alignment
+                ]
+
+            ctc_limit, softmax, raw, targets, _ = g.sess.run(
+                train_ops,
+                feed_dict=b.feeds.alignments
+            )
+
+            # TODO: Need to test whether this gets the correct decoding output
+            #       as i finally got the batch decoder working!
+            # TODO: Could we look to MAXIMISE a target alignment? i.e. maximise
+            #       the negative log likelihood for an alignment
+            # TODO: Could we try to find an adversarial alignment? i.e. where
+            #       there are no repeat characters, only characters from the
+            #       target transcription
+            #       ooooooooopppppppeeeeeeennnnnn vs o------------p------e---n
+
+            #print(np.argmax(softmax, axis=2))
+
+            decodings, probs = victim.inference(
+                b,
+                logits=softmax,
+                decoder="batch",
+                top_five=False
+            )
+            print(decodings, probs)
+
+            if all([0.1 > c for c in ctc_limit]):
+                break
+            #if all([d == b.targets.phrases[0] for d in decodings]):
+            #    break
+            #    #self.target_alignments = np.argmax(softmax,axis=2)
+
+
+class CTCAlignmentsUpdateHard(Base):
+    def __init__(self, attack, alignment_graph, *args, loss_lower_bound=10.0, **kwargs):
+        """
+        Initialise the evaluation procedure.
+
+        :param attack_graph: The current attack graph perform optimisation with.
+        """
+
+        super().__init__(attack, *args, **kwargs)
+
+        self.loss_bound = loss_lower_bound
+
+        self.alignment_graph = alignment_graph
+
+        # We must wait until now to initialise the optimiser so that we can
+        # initialise only the attack variables (i.e. not the deepspeech ones).
+        start_vars = set(x.name for x in tf.global_variables())
+
+        self.alignment_graph.optimiser.create_optimiser()
+
+        attack.optimiser.create_optimiser()
+
+        end_vars = tf.global_variables()
+        new_vars = [x for x in end_vars if x.name not in start_vars]
+
+        # TODO: New base class that holds vars to be *initialised* rather than opt_vars attributes.
+        attack.sess.run(tf.variables_initializer(new_vars + attack.graph.opt_vars + [alignment_graph.graph.raw_alignments]))
+
+    def run(self):
+        """
+        Do the actual optimisation.
+        TODO: Distance metric could be held in AttackGraph or Base?
+
+        :param batch: batch data for the specified model.
+        :param steps: total number of steps to run optimisation for.
+        :param decode_step: when to check for a successful decoding.
+        :return:
+            stats for each step of optimisation (loss measurements),
+            successful adversarial examples
+        """
+        attack, g, b = self.attack, self.attack.graph, self.attack.batch
+
+        self.alignment_graph.optimise(self.attack.victim)
+
+        while self.current_step < self.steps:
+
+            self.current_step += 1
+            step = self.current_step
+
+            attack.optimiser.optimise(b.feeds.attack)
+
+            if step % self.decode_step == 0:
+                # can use either tf or deepspeech decodings
+                # we prefer ds as it's what the actual model would use.
+                # It does mean switching to CPU every time we want to do
+                # inference but there's not a major hit to performance
+
+                decodings, probs = attack.victim.inference(
+                    b,
+                    feed=b.feeds.attack,
+                    decoder="batch",
+                    top_five=False,
+                )
+
+                outs = {
+                    "step": self.current_step,
+                    "data": [],
+                }
+
+                targets = b.targets.phrases
+
+                loss = self.tf_run(attack.adversarial_loss.loss_fn)
+                target_loss = [self.loss_bound for _ in range(b.size)]
+
+                for idx, success in self.update_success(loss, target_loss):
+
+                    out = {
+                        "idx": idx,
+                        "success": success,
+                        "decodings": decodings[idx],
+                        "target_phrase": b.targets.phrases[idx],
+                        "probs": probs
+                    }
+
+                    outs["data"].append(out)
+
+                yield outs
+
