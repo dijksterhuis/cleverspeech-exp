c44750793bbdd57b2ca1257c994363c6bd86164d
dijksterhuis <mrobeson@dundee.ac.uk>
2021-02-09 20:50:24 +0000

    ADD: New attacks code for adaptive kappa max diff (CW) loss function, plus code to do dense and sparse alignments.

--- Confidence/attacks.py
+++ Confidence/attacks.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 import os
 
+from cleverspeech.data import Generators
+from cleverspeech.data import Feeds
 from cleverspeech.graph.GraphConstructor import Constructor
 from cleverspeech.graph import Constraints
+from cleverspeech.graph import Graphs
+from cleverspeech.graph import Losses
 from cleverspeech.graph import Optimisers
 from cleverspeech.graph import Procedures
 from cleverspeech.graph import Outputs
@@ -25,8 +29,8 @@ SPAWN_DELAY = 30
 TOKENS = " abcdefghijklmnopqrstuvwxyz'-"
 BEAM_WIDTH = 500
 
-INDIR = "/data/target-logits/"
-OUTDIR = "/data/adv/hi-scores/"
+INDIR = "./target-logits/"
+OUTDIR = "./adv/confidence/"
 
 # targets search parameters
 MAX_EXAMPLES = 1000
@@ -34,7 +38,7 @@ MAX_EXAMPLES = 1000
 RESCALE = 0.95
 CONSTRAINT_UPDATE = "geom"
 LEARNING_RATE = 10
-NUMB_STEPS = 10000
+NUMB_STEPS = 20000
 DECODING_STEP = 10
 BATCH_SIZE = 10
 LOGITS_SEARCH = "sbgs"
@@ -42,31 +46,13 @@ LOGITS_SEARCH = "sbgs"
 N_RUNS = 1
 
 
-"""
-Tips on new attack definitions:
-
-0. Prefer to make changes to in ./experiments first, instead of jumping straight 
-into changing ./cleverspeech/Attacks  
-
-1. Get `create_attack_graph` function working first, then worry about settings 
--- you can always perform hyperparameter searches.
-
-2. Do you need to think a bit harder about how the rest of the code structure? 
-Don't be afraid to change things -- it's all a learning process!
-
-3. Keep everything in a settings dictionary to explicitly document exact 
-parameters used for experiments.
-
-4.  If something with attack classes / objects needs to change, don't hack it.
-Think about the change and refactor anything that needs refactoring. You'll
-thank me in the long run.
-
-5. Procedures are a bit f**ked at the moment. I need to have a think on this.  
-
-"""
+AUDIOS_INDIR = "./samples/all/"
+TARGETS_PATH = "./samples/cv-valid-test.csv"
+MAX_TARGETS = 1000
+MAX_AUDIO_LENGTH = 120000
 
 
-def get_batch_factory(settings):
+def get_sbgs_batch_factory(settings):
     # get a N samples of all the data
 
     target_logits_etl = custom_etl.AllTargetLogitsAndAudioFilePaths(
@@ -91,13 +77,7 @@ def get_batch_factory(settings):
 
 def squared_diff_loss(master_settings):
     """
-    Creates some target logits that fills as much of the resulting alignment
-    with repeated tokens. These tokens are repeated in the merge step, but it
-    makes the decoded transcription score *way* higher.
-
-    Unfortunately we have to balance the decoder confidence score against the
-    ability to find a suitable solution as using n_repeats > 5 will probably
-    take a lot of steps to optimise.
+    Non functioning.
 
     :return: None
     """
@@ -167,7 +147,7 @@ def squared_diff_loss(master_settings):
         }
         settings.update(master_settings)
 
-        batch_factory = get_batch_factory(settings)
+        batch_factory = get_sbgs_batch_factory(settings)
         batch_gen = batch_factory.generate(
             ETL.AudioExamples, custom_etl.TargetLogits, custom_defs.HiScoresFeed
         )
@@ -177,10 +157,445 @@ def squared_diff_loss(master_settings):
         log("Finished run {}.".format(run))
 
 
+def get_normal_batch_generator(settings):
+
+    # get N samples of all the data. alsp make sure to limit example length,
+    # otherwise we'd have to do adaptive batch sizes.
+
+    audio_etl = ETL.AllAudioFilePaths(
+        settings["audio_indir"],
+        settings["max_examples"],
+        filter_term=".wav",
+        max_samples=settings["max_audio_length"]
+    )
+
+    all_audio_file_paths = audio_etl.extract().transform().load()
+
+    targets_etl = ETL.AllTargetPhrases(
+        settings["targets_path"], settings["max_targets"],
+    )
+    all_targets = targets_etl.extract().transform().load()
+
+    # Generate the batches in turn, rather than all in one go ...
+
+    batch_factory = Generators.BatchGenerator(
+        all_audio_file_paths, all_targets, settings["batch_size"]
+    )
+
+    # ... To save resources by only running the final ETLs on a batch of data
+
+    batch_gen = batch_factory.generate(
+        ETL.AudioExamples, ETL.TargetPhrases, Feeds.Attack
+    )
+
+    log(
+        "New Run",
+        "Number of test examples: {}".format(batch_factory.numb_examples),
+        ''.join(["{k}: {v}\n".format(k=k, v=v) for k, v in settings.items()]),
+    )
+    return batch_gen
+
+
+def adaptive_kappa_ctc_sparse_run(master_settings):
+    """
+    MRMaxDiff (extension of CWMaxDiff) with CTCLoss and sparse alignment.
+
+    Aim: Optimise MRMaxDiff whilst regularising with CTCLoss. It seems that this
+    actually does the opposite, using MRMaxDiff as a regulariser for CTCLoss to
+    force one the specific alignment.
+
+    :return: None
+    """
+    def create_attack_graph(sess, batch, settings):
+        attack = Constructor(sess, batch)
+
+        attack.add_hard_constraint(
+            Constraints.L2,
+            r_constant=settings["rescale"],
+            update_method=settings["constraint_update"],
+        )
+
+        attack.add_graph(
+            Graphs.SimpleAttack
+        )
+
+        attack.add_victim(
+            DeepSpeech.Model,
+            tokens=settings["tokens"],
+            decoder=settings["decoder_type"],
+            beam_width=settings["beam_width"]
+        )
+
+        alignment = Constructor(attack.sess, batch)
+        alignment.add_graph(custom_defs.CTCSearchGraph, attack)
+        alignment.add_loss(custom_defs.AlignmentLoss)
+        alignment.create_loss_fn()
+        alignment.add_optimiser(custom_defs.CTCAlignmentOptimiser)
+
+        attack.add_adversarial_loss(
+            custom_defs.AdaptiveKappaCWMaxDiff,
+            alignment.graph.target_alignments,
+            k=settings["kappa"],
+        )
+        attack.add_distance_loss(
+            Losses.CTCLoss,
+            loss_weight=1.0,
+        )
+        attack.create_loss_fn()
+        attack.add_optimiser(
+            Optimisers.AdamOptimiser,
+            learning_rate=settings["learning_rate"]
+        )
+        attack.add_procedure(
+            custom_defs.CTCAlignmentsUpdateOnDecode,
+            alignment_graph=alignment,
+            steps=settings["nsteps"],
+            decode_step=settings["decode_step"]
+        )
+        attack.add_outputs(
+            Outputs.Base,
+            settings["outdir"],
+        )
+
+        return attack
+
+    kappa = 0.1
+
+    outdir = os.path.join(OUTDIR, "adaptivekappa-ctcmaxdiff-ctc-sparse/")
+    outdir = os.path.join(outdir, "kappa_{}/".format(kappa))
+
+    settings = {
+        "audio_indir": AUDIOS_INDIR,
+        "targets_path": TARGETS_PATH,
+        "outdir": outdir,
+        "batch_size": BATCH_SIZE,
+        "tokens": TOKENS,
+        "nsteps": NUMB_STEPS,
+        "decode_step": DECODING_STEP,
+        "beam_width": BEAM_WIDTH,
+        "constraint_update": CONSTRAINT_UPDATE,
+        "rescale": RESCALE,
+        "learning_rate": LEARNING_RATE,
+        "gpu_device": GPU_DEVICE,
+        "max_spawns": MAX_PROCESSES,
+        "spawn_delay": SPAWN_DELAY,
+        "kappa": float(kappa),
+        "decoder_type": "batch",
+        "max_examples": MAX_EXAMPLES,
+        "max_targets": MAX_TARGETS,
+        "max_audio_length": MAX_AUDIO_LENGTH,
+    }
+
+    settings.update(master_settings)
+    batch_gen = get_normal_batch_generator(settings)
+
+    execute(settings, create_attack_graph, batch_gen)
+
+    log("Finished run {}.".format(kappa))
+
+
+def adaptive_kappa_sparse_run(master_settings):
+    """
+    MRMaxDiff (extension of CWMaxDiff) with with a sparse alignment.
+    :return: None
+    """
+    def create_attack_graph(sess, batch, settings):
+        attack = Constructor(sess, batch)
+
+        attack.add_hard_constraint(
+            Constraints.L2,
+            r_constant=settings["rescale"],
+            update_method=settings["constraint_update"],
+        )
+
+        attack.add_graph(
+            Graphs.SimpleAttack
+        )
+
+        attack.add_victim(
+            DeepSpeech.Model,
+            tokens=settings["tokens"],
+            decoder=settings["decoder_type"],
+            beam_width=settings["beam_width"]
+        )
+
+        alignment = Constructor(attack.sess, batch)
+        alignment.add_graph(custom_defs.CTCSearchGraph, attack)
+        alignment.add_adversarial_loss(custom_defs.AlignmentLoss)
+        alignment.create_loss_fn()
+        alignment.add_optimiser(custom_defs.CTCAlignmentOptimiser)
+
+        attack.add_adversarial_loss(
+            custom_defs.AdaptiveKappaCWMaxDiff,
+            alignment.graph.target_alignments,
+            k=settings["kappa"]
+        )
+        attack.create_loss_fn()
+        attack.add_optimiser(
+            Optimisers.AdamOptimiser,
+            learning_rate=settings["learning_rate"]
+        )
+        attack.add_procedure(
+            custom_defs.CTCAlignmentsUpdateOnDecode,
+            alignment_graph=alignment,
+            steps=settings["nsteps"],
+            decode_step=settings["decode_step"]
+        )
+        attack.add_outputs(
+            Outputs.Base,
+            settings["outdir"],
+        )
+
+        return attack
+
+    kappa = 0.1
+
+    outdir = os.path.join(OUTDIR, "adaptivekappa-ctcmaxdiff-sparse/")
+    outdir = os.path.join(outdir, "kappa_{}/".format(kappa))
+
+    settings = {
+        "audio_indir": AUDIOS_INDIR,
+        "targets_path": TARGETS_PATH,
+        "outdir": outdir,
+        "batch_size": BATCH_SIZE,
+        "tokens": TOKENS,
+        "nsteps": NUMB_STEPS,
+        "decode_step": DECODING_STEP,
+        "beam_width": BEAM_WIDTH,
+        "constraint_update": CONSTRAINT_UPDATE,
+        "rescale": RESCALE,
+        "learning_rate": LEARNING_RATE,
+        "gpu_device": GPU_DEVICE,
+        "max_spawns": MAX_PROCESSES,
+        "spawn_delay": SPAWN_DELAY,
+        "kappa": float(kappa),
+        "decoder_type": "batch",
+        "max_examples": MAX_EXAMPLES,
+        "max_targets": MAX_TARGETS,
+        "max_audio_length": MAX_AUDIO_LENGTH,
+    }
+
+    settings.update(master_settings)
+    batch_gen = get_normal_batch_generator(settings)
+
+    execute(settings, create_attack_graph, batch_gen)
+
+    log("Finished run {}.".format(kappa))
+
+
+def get_dense_batch_factory(settings):
+
+    # get N samples of all the data. alsp make sure to limit example length,
+    # otherwise we'd have to do adaptive batch sizes.
+
+    audio_etl = ETL.AllAudioFilePaths(
+        settings["audio_indir"],
+        settings["max_examples"],
+        filter_term=".wav",
+        max_samples=settings["max_audio_length"]
+    )
+
+    all_audio_file_paths = audio_etl.extract().transform().load()
+
+    targets_etl = ETL.AllTargetPhrases(
+        settings["targets_path"], settings["max_targets"],
+    )
+    all_targets = targets_etl.extract().transform().load()
+
+    # Generate the batches in turn, rather than all in one go ...
+
+    batch_factory = custom_etl.CTCHiScoresBatchGenerator(
+        all_audio_file_paths, all_targets, settings["batch_size"]
+    )
+
+    # ... To save resources by only running the final ETLs on a batch of data
+
+    batch_gen = batch_factory.generate(
+        ETL.AudioExamples, custom_etl.RepeatsTargetPhrases, Feeds.Attack
+    )
+
+    log(
+        "New Run",
+        "Number of test examples: {}".format(batch_factory.numb_examples),
+        ''.join(["{k}: {v}\n".format(k=k, v=v) for k, v in settings.items()]),
+    )
+    return batch_gen
+
+
+def adaptive_kappa_ctc_dense_run(master_settings):
+    """
+    Currently broken.
+    """
+    def create_attack_graph(sess, batch, settings):
+        attack = Constructor(sess, batch)
+
+        attack.add_hard_constraint(
+            Constraints.L2,
+            r_constant=settings["rescale"],
+            update_method=settings["constraint_update"],
+        )
+
+        attack.add_graph(
+            Graphs.SimpleAttack
+        )
+
+        attack.add_victim(
+            DeepSpeech.Model,
+            tokens=settings["tokens"],
+            decoder=settings["decoder_type"],
+            beam_width=settings["beam_width"]
+        )
+
+        attack.add_adversarial_loss(
+            custom_defs.AdaptiveKappaCWMaxDiff,
+            attack.graph.placeholders.targets,
+            k=settings["kappa"]
+        )
+        attack.add_distance_loss(
+            Losses.CTCLoss,
+            loss_weight=1.0,
+        )
+        attack.create_loss_fn()
+        attack.add_optimiser(
+            Optimisers.AdamOptimiser,
+            learning_rate=settings["learning_rate"]
+        )
+        attack.add_procedure(
+            Procedures.UpdateOnDecoding,
+            steps=settings["nsteps"],
+            decode_step=settings["decode_step"]
+        )
+        attack.add_outputs(
+            Outputs.Base,
+            settings["outdir"],
+        )
+
+        return attack
+
+    kappa = 0.1
+
+    outdir = os.path.join(OUTDIR, "adaptivekappa-ctcmaxdiff-ctc-dense/")
+    outdir = os.path.join(outdir, "kappa_{}/".format(kappa))
+
+    settings = {
+        "audio_indir": AUDIOS_INDIR,
+        "targets_path": TARGETS_PATH,
+        "outdir": outdir,
+        "batch_size": BATCH_SIZE,
+        "tokens": TOKENS,
+        "nsteps": NUMB_STEPS,
+        "decode_step": DECODING_STEP,
+        "beam_width": BEAM_WIDTH,
+        "constraint_update": CONSTRAINT_UPDATE,
+        "rescale": RESCALE,
+        "learning_rate": LEARNING_RATE,
+        "gpu_device": GPU_DEVICE,
+        "max_spawns": MAX_PROCESSES,
+        "spawn_delay": SPAWN_DELAY,
+        "kappa": float(kappa),
+        "decoder_type": "batch",
+        "max_examples": MAX_EXAMPLES,
+        "max_targets": MAX_TARGETS,
+        "max_audio_length": MAX_AUDIO_LENGTH,
+    }
+
+    settings.update(master_settings)
+    batch_gen = get_dense_batch_factory(settings)
+
+    execute(settings, create_attack_graph, batch_gen)
+
+    log("Finished run {}.".format(kappa))
+
+
+def adaptive_kappa_dense_run(master_settings):
+    """
+    MRMaxDiff (extension of CWMaxDiff) with a dense alignment.
+    """
+    def create_attack_graph(sess, batch, settings):
+        attack = Constructor(sess, batch)
+
+        attack.add_hard_constraint(
+            Constraints.L2,
+            r_constant=settings["rescale"],
+            update_method=settings["constraint_update"],
+        )
+
+        attack.add_graph(
+            Graphs.SimpleAttack
+        )
+
+        attack.add_victim(
+            DeepSpeech.Model,
+            tokens=settings["tokens"],
+            decoder=settings["decoder_type"],
+            beam_width=settings["beam_width"]
+        )
+
+        attack.add_adversarial_loss(
+            custom_defs.AdaptiveKappaCWMaxDiff,
+            attack.graph.placeholders.targets,
+            k=settings["kappa"]
+        )
+        attack.create_loss_fn()
+        attack.add_optimiser(
+            Optimisers.AdamOptimiser,
+            learning_rate=settings["learning_rate"]
+        )
+        attack.add_procedure(
+            Procedures.UpdateOnDecoding,
+            steps=settings["nsteps"],
+            decode_step=settings["decode_step"]
+        )
+        attack.add_outputs(
+            Outputs.Base,
+            settings["outdir"],
+        )
+
+        return attack
+
+    kappa = 0.25
+
+    outdir = os.path.join(OUTDIR, "adaptivekappa-ctcmaxdiff-dense/")
+    outdir = os.path.join(outdir, "kappa_{}/".format(kappa))
+
+    settings = {
+        "audio_indir": AUDIOS_INDIR,
+        "targets_path": TARGETS_PATH,
+        "outdir": outdir,
+        "batch_size": BATCH_SIZE,
+        "tokens": TOKENS,
+        "nsteps": NUMB_STEPS,
+        "decode_step": DECODING_STEP,
+        "beam_width": BEAM_WIDTH,
+        "constraint_update": CONSTRAINT_UPDATE,
+        "rescale": RESCALE,
+        "learning_rate": LEARNING_RATE,
+        "gpu_device": GPU_DEVICE,
+        "max_spawns": MAX_PROCESSES,
+        "spawn_delay": SPAWN_DELAY,
+        "kappa": float(kappa),
+        "decoder_type": "batch",
+        "max_examples": MAX_EXAMPLES,
+        "max_targets": MAX_TARGETS,
+        "max_audio_length": MAX_AUDIO_LENGTH,
+    }
+
+    settings.update(master_settings)
+    batch_gen = get_dense_batch_factory(settings)
+
+    execute(settings, create_attack_graph, batch_gen)
+
+    log("Finished run {}.".format(kappa))
+
+
 if __name__ == '__main__':
 
     experiments = {
         "squared_diff_loss": squared_diff_loss,
+        "sparse-adaptivekappa-ctc-maxdiff": adaptive_kappa_ctc_sparse_run,
+        "dense-adaptivekappa-ctc-maxdiff": adaptive_kappa_ctc_dense_run,
+        "sparse-adaptivekappa-maxdiff": adaptive_kappa_sparse_run,
+        "dense-adaptivekappa-maxdiff": adaptive_kappa_dense_run,
     }
 
     args(experiments)
--- Confidence/custom_etl.py
+++ Confidence/custom_etl.py
@@ -3,8 +3,9 @@ import json
 import numpy as np
 
 from cleverspeech.data.Batches import Batch
-from cleverspeech.data.ETL import ETL
-from cleverspeech.utils.Utils import np_arr, np_zero, lcomp, load_wavs, l_map
+from cleverspeech.data.ETL import ETL, TargetPhrases
+from cleverspeech.data.Generators import BaseBatchGenerator, pop_target_phrase
+from cleverspeech.utils.Utils import np_arr, np_zero, lcomp, load_wavs, l_map, assert_positive_int
 
 
 class TargetLogitsBatch(object):
@@ -283,3 +284,186 @@ class LogitsBatchGenerator:
 
             yield self.id, self.batch
 
+
+def calculate_possible_repeats(actual_n_feats, target_phrase_length):
+    return actual_n_feats // target_phrase_length
+
+
+def insert_target_blanks(target_indices):
+    # get a shifted list so we can compare back one step in the phrase
+
+    previous_indices = target_indices.tolist()
+    previous_indices.insert(0, None)
+
+    # insert blank tokens where ctc would expect them - i.e. `do-or`
+    # also insert a blank at the start (it gives space for the RNN to "warm up")
+    with_repeats = [28]
+    for current, previous in zip(target_indices, previous_indices):
+        if not previous:
+            with_repeats.append(current)
+        elif current == previous:
+            with_repeats.append(28)
+            with_repeats.append(current)
+        else:
+            with_repeats.append(current)
+    return with_repeats
+
+
+def create_new_target_indices(new_target, n_feats, repeats):
+
+    """
+    Taking into account the space we have available, find out the new argmax
+    indices for each frame of audio which relate to our target phrase
+
+    :param new_target: the new target phrase included additional blank tokens
+    :param n_feats: the number of features in the logits (time steps)
+    :param repeats: the number of repeats for each token
+
+    :return: the index for each frame in turn
+    """
+
+    spacing = n_feats // new_target.size
+
+    for t in new_target:
+        for i in range(spacing):
+            if i > repeats:
+                yield 28
+            else:
+                yield t
+
+
+def pad_indices(indices, act_len):
+    n_paddings = act_len - len(indices)
+    padded = np.concatenate([indices, np.ones(n_paddings + 1) * 28])
+    return padded
+
+
+class CTCHiScoresBatchGenerator(BaseBatchGenerator):
+    def __init__(self, all_audio_file_paths, all_targets, batch_size):
+
+        numb_examples = len(all_audio_file_paths)
+        numb_targets = len(all_targets)
+
+        assert_positive_int(batch_size)
+        assert_positive_int(numb_examples)
+        assert_positive_int(numb_targets)
+
+        assert numb_targets * batch_size >= numb_examples
+
+        self.all_audio_file_paths = all_audio_file_paths
+        self.all_targets = all_targets
+
+        super().__init__(batch_size, numb_examples)
+
+    def generate(self, audio_etl_cls, target_etl_cls, feeds_cls):
+
+        for idx in range(0, self.numb_examples, self.size):
+
+            # Handle remainders: number of examples // desired batch size != 0
+            if len(self.all_audio_file_paths) < self.size:
+                self.size = len(self.all_audio_file_paths)
+
+            # get n files paths and create the audio batch data
+            audio_batch_data = self.popper(self.all_audio_file_paths)
+            audios = audio_etl_cls(audio_batch_data)
+            audio_batch = audios.extract().transform().load()
+
+            #  we need to make sure target phrase length < n audio feats.
+            # also, the first batch should also have the longest target phrase
+            # and longest audio examples so we can easily manage GPU Memory
+            # resources with the AttackSpawner context manager.
+
+            actual_n_feats = audio_batch.alignment_lengths
+
+            # ensure we get *at least* duplicate characters by dividing the
+            # minimum possible length by 3
+            target_phrase = pop_target_phrase(
+                self.all_targets, min(actual_n_feats) // 3
+            )
+
+            # each target must be the same length else numpy throws a hissyfit
+            # because it can't understand skewed matrices
+            target_batch_data = l_map(
+                lambda _: target_phrase, range(self.size)
+            )
+
+            # actually load the n phrases as a batch of target data
+            targets = target_etl_cls(target_batch_data, actual_n_feats)
+            target_batch = targets.extract().transform().load()
+
+            self.batch = Batch(
+                self.size,
+                audio_batch,
+                target_batch,
+                feeds_cls
+            )
+
+            self.id = idx
+
+            yield self.id, self.batch
+
+
+class RepeatsTargetPhrases(TargetPhrases):
+    def __init__(self, data, n_feats, tokens=" abcdefghijklmnopqrstuvwxyz'-"):
+
+        self.__n_feats = n_feats
+        super().__init__(data, tokens=tokens)
+
+    def extract(self):
+        return super().extract()
+
+    def transform(self):
+        super().transform()
+
+        # hacky access to parent class
+        transformed = getattr(self, "_TargetPhrases__transformed")
+
+        new_indices = np_arr(
+            l_map(
+                lambda x: insert_target_blanks(x),
+                transformed[2]
+            ),
+            np.int32
+        )
+        # calculate the actual number of repeats
+        z = zip(self.__n_feats, transformed[3])
+        n_repeats = [calculate_possible_repeats(x, y) for x, y in z]
+
+        # do linear expansion only on the existing indices (target phrases
+        # are still valid as they are).
+        z = zip(new_indices, self.__n_feats, n_repeats)
+        transformed[2] = np_arr(
+            [
+                l_map(
+                    lambda x: x,
+                    create_new_target_indices(x, y, z)
+                ) for x, y, z in z
+            ],
+            np.int32
+        )
+
+        # do padding for nopn-ctc loss
+        z = zip(transformed[2], self.__n_feats)
+        transformed[2] = np_arr(
+            [
+                l_map(
+                    lambda x: x,
+                    pad_indices(x, y)
+                ) for x, y in z
+            ],
+            np.int32
+        )
+
+        # update the target sequence lengths
+        transformed[3] = l_map(
+            lambda x: x.size,
+            transformed[2]
+        )
+
+        # hacky access to parent class
+        setattr(self, "_TargetPhrases__transformed", transformed)
+
+        return self
+
+    def load(self):
+        return super().load()
