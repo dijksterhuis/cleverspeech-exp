dfeb79f1d21ee045ba26709cd8f2697a19bfcbd5
dijksterhuis <mrobeson@dundee.ac.uk>
2021-03-15 22:40:24 +0000

    UPDATE: Make sure all confidence attacks write results to the ./adv/confidence/ directory.

--- Confidence/InvertedCTC/attacks.py
+++ Confidence/InvertedCTC/attacks.py
@@ -29,7 +29,7 @@ SPAWN_DELAY = 30
 
 AUDIOS_INDIR = "./samples/all/"
 TARGETS_PATH = "./samples/cv-valid-test.csv"
-OUTDIR = "./adv/antictc/"
+OUTDIR = "./adv/confidence/antictc/"
 MAX_EXAMPLES = 100
 MAX_TARGETS = 1000
 MAX_AUDIO_LENGTH = 120000
