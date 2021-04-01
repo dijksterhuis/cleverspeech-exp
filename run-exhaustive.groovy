9b0f9bd993ab49931bb2a0e9f3cc1f6399edf422
dijksterhuis <mrobeson@dundee.ac.uk>
2021-04-01 03:26:29 +0100

    Add an "exhaustive" run jenkins script in case the shared GPUs suddenly become free for ages (or I get some cash for all the AWS machines)

---
+++ run-exhaustive.groovy
@@ -0,0 +1,82 @@
+#!/usr/bin/env groovy
+
+pipeline {
+    agent { label 'build' }
+    parameters {
+
+        string name: 'MAX_SPAWNS',
+            defaultValue: '3',
+            description: 'Number of attacks to allow to spawn at once.'
+
+        string name: 'BATCH_SIZE',
+            defaultValue: '10',
+            description: 'How many examples in a batch.'
+
+        string name: 'N_STEPS',
+            defaultValue: '25000',
+            description: 'How many iterations to run the attack for.'
+        /*
+        choice name: 'EXP_SCRIPT',
+            choices: ['attacks', 'unbounded'],
+            description: 'Which attack python script to run. default: attacks.py.'
+
+        choice name: 'DATA',
+            choices: ['samples', 'silence'],
+            description: 'Which dataset to use. default: ./samples'
+        */
+
+        text   name: 'ADDITIONAL_ARGS',
+            defaultValue: '',
+            description: 'Additional arguments to pass to the attack script e.g. --decode_step 10. default: none.'
+
+    }
+    stages {
+        stage("Run all the experiments!"){
+            failFast false
+            matrix {
+                axes {
+                    axis {
+                        name 'DIR'
+                        values 'baseline-ctc',
+                            'baseline-cwmaxdff',
+                            'conf-adaptivekappa',
+                            'conf-alignmentedgecases',
+                            'conf-invertedctc',
+                            'conf-logprobsgreedydiff',
+                            'conf-sumlogprobs',
+                            'conf-cumulativelogprobs' /*,
+                             'percep-synthesis',
+                             'percep-synthesisregularised',
+                             'percep-spectralloss'
+                             */
+                    }
+                    axis {
+                        name 'MATRIX_DATA'
+                        values 'samples', 'silence'
+                    }
+                    axis {
+                        name 'MATRIX_EXP_SCRIPT'
+                        values 'attacks', 'unbounded'
+                    }
+                }
+                stages {
+                    stage("Run experiment") {
+                        steps {
+                            echo "Starting ${DIR} build job..."
+                            build job: "${DIR}",
+                                wait: true,
+                                parameters: [
+                                    stringParam(name: 'ADDITIONAL_ARGS', value: "${params.ADDITIONAL_ARGS}"),
+                                    stringParam(name: 'EXP_SCRIPT', value: "${MATRIX_EXP_SCRIPT}"),
+                                    stringParam(name: 'MAX_SPAWNS', value: "${params.MAX_SPAWNS}"),
+                                    stringParam(name: 'N_STEPS', value: "${params.N_STEPS}"),
+                                    stringParam(name: 'DATA', value: "${MATRIX_DATA}"),
+                                    stringParam(name: 'JOB_TYPE', value: "run"),
+                                ]
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
