f82b73e6ca3f673d95afaa3ae250a1f59541549e
dijksterhuis <mrobeson@dundee.ac.uk>
2021-05-05 01:38:25 +0100

    UPDATE: Add loss to the jenkins script as a hyper-parameter.

--- misc-batch-vs-indy/run.groovy
+++ misc-batch-vs-indy/run.groovy
@@ -100,7 +100,8 @@ pipeline {
                 STEPS_ARG="--nsteps ${params.N_STEPS}"
                 BATCH_ARG="--nbatch_max ${params.MAX_BATCH_SIZE} --nbatch_step ${params.STEP_BATCH_SIZE}"
                 GRAPH_ARG="--graph \${GRAPH}"
-                PY_EXP_ARGS="${SPAWN_ARG} ${BATCH_ARG} ${STEPS_ARG} ${GRAPH_ARG}"
+                LOSS_ARG="--loss \${LOSS}"
+                PY_EXP_ARGS="${SPAWN_ARG} ${BATCH_ARG} ${STEPS_ARG} ${GRAPH_ARG} ${LOSS_ARG}"
 
                 PYTHON_CMD = "${PY_BASE_CMD} ${PY_EXP_ARGS} ${PY_DATA_ARGS} ${params.ADDITIONAL_ARGS}"
             }
@@ -113,6 +114,10 @@ pipeline {
                     axis {
                         name 'GRAPH'
                         values 'batch', 'indy'
+                    }
+                    axis {
+                        name 'LOSS'
+                        values 'ctc', 'ctc2'
                     }
                 }
                 stages {
