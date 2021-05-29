f1df13de15328fbeb2a6f0fe0ab5ec4def5ae76d
dijksterhuis <mrobeson@dundee.ac.uk>
2021-05-29 21:51:08 +0100

    ADD: New attack.

---
+++ conf-weightedmaxmin/run.groovy
@@ -0,0 +1,202 @@
+#!/usr/bin/env groovy
+
+pipeline {
+    /* Use jenkins build node to manage how many experiments to run at a time. */
+    agent {
+        label "build"
+    }
+    options {
+        skipDefaultCheckout()
+        timestamps()
+        disableResume()
+        disableConcurrentBuilds()
+    }
+    /*
+    triggers {
+        pollSCM('H H * * 1-5') }
+        upstream(upstreamProjects: './build/latest', threshold: hudson.model.Result.SUCCESS) }
+    }
+    */
+    environment {
+        EXP_BASE_NAME = "conf-biggiomaxofmaxmin"
+        IMAGE = "dijksterhuis/cleverspeech:latest"
+    }
+    parameters {
+
+            /*
+            The first three parameters are priorities when deploying, as the "gpu" jenkins agents
+            are shared resources and these parameters will determine whether jobs fail or not due to
+            resource issues (basically, undergrads might be hogging GPU memory).
+            */
+
+
+            string name: 'N_STEPS',
+                defaultValue: '10000',
+                description: 'How many iterations to run the attack for.'
+
+            string name: 'BATCH_SIZE',
+                defaultValue: '10',
+                description: 'How many examples in a batch.'
+
+            /*
+            The next 4 parameters are used to determine how to run the attacks. These parameters are
+            generally sort-of hyper parameters (very sort of).
+            */
+            choice name: 'JOB_TYPE',
+                choices: ['run', 'test'],
+                description: 'Whether this is an experiment run or if we are just testing that everything works. default: run.'
+
+            choice name: 'EXP_SCRIPT',
+                choices: ['attacks', 'unbounded'],
+                description: 'Which attack python script to run. default: attacks.py.'
+
+            choice name: 'DATA',
+                choices: ['samples', 'silence'],
+                description: 'Which dataset to use. default: ./samples'
+
+            choice name: 'WRITER',
+                choices: ['local', 's3'],
+                description: 'How/where to write results data?. default: local.'
+
+            choice name: 'ALIGNMENT_FILTER',
+                choices: ['all', 'dense', 'ctcalign', 'sparse'],
+                description: 'Filter experiments based on alignment hyper parameter. Note that this only works when combined with other filters.'
+
+            choice name: 'DECODER_FILTER',
+                choices: ['all', 'batch', 'greedy'],
+                description: 'Filter experiments based on decoder hyper parameter. Note that this only works when combined with other filters.'
+
+            text   name: 'ADDITIONAL_ARGS',
+                defaultValue: '',
+                description: 'Additional arguments to pass to the attack script e.g. --decode_step 10. default: none.'
+    }
+
+    stages {
+        stage("Modify jenkins build information") {
+            steps {
+                script {
+                    def name = "#${BUILD_ID}: type:${params.JOB_TYPE} script:${params.EXP_SCRIPT} data:${params.DATA} steps:${params.N_STEPS}"
+                    buildName "${name}"
+                }
+            }
+        }
+        stage("Locked SCM checkout") {
+            steps {
+                lock("dummy") {
+                    sleep 5
+                    checkout scm
+                }
+            }
+        }
+        stage("Run combos in parallel."){
+            failFast false /* If one run fails, keep going! */
+            environment{
+                /*
+                Nasty way of not-really-but-sort-of simplifying the mess of our docker run command
+                */
+                DOCKER_NAME="${EXP_BASE_NAME}-${EXP_SCRIPT}-${DATA}-\${ALIGNMENT}-\${DECODER}-${JOB_TYPE}"
+                DOCKER_MOUNT="\$(pwd)/${BUILD_ID}:/home/cleverspeech/cleverSpeech/adv/"
+                DOCKER_UID="LOCAL_UID=\$(id -u ${USER})"
+                DOCKER_GID="LOCAL_GID=\$(id -g ${USER})"
+                AWS_ACCESS_KEY_ID = credentials('jenkins-aws-secret-key-id')
+                AWS_SECRET_ACCESS_KEY = credentials('jenkins-aws-secret-access-key')
+
+                PY_BASE_CMD="python3 ./experiments/${EXP_BASE_NAME}/${params.EXP_SCRIPT}.py"
+                PY_DATA_ARGS="--audio_indir ./${params.DATA}/all/ --targets_path ./${params.DATA}/cv-valid-test.csv --outdir ./adv/${BUILD_ID}/${JOB_TYPE}"
+
+
+                STEPS_ARG="--nsteps ${params.N_STEPS}"
+                BATCH_ARG="--batch_size ${params.BATCH_SIZE}"
+                ALIGN_ARG="--align \${ALIGNMENT}"
+                DECODER_ARG="--decoder \${DECODER}"
+                WRITER_ARG="--writer ${params.WRITER}"
+                PY_EXP_ARGS="${WRITER_ARG} ${BATCH_ARG} ${BATCH_ARG} ${STEPS_ARG} ${ALIGN_ARG} ${DECODER_ARG}"
+
+                PYTHON_CMD = "${PY_BASE_CMD} ${PY_EXP_ARGS} ${PY_DATA_ARGS} ${params.ADDITIONAL_ARGS}"
+            }
+            matrix {
+                /* Run each of these combinations over all axes on the gpu machines. */
+                agent {
+                    label "gpu"
+                }
+                when {
+                    anyOf {
+                        allOf{
+                            /* no filters applied so run everything */
+                            expression { params.ALIGNMENT_FILTER == 'all' }
+                            expression { params.LOSS_FILTER == 'all' }
+                            expression { params.DECODER_FILTER == 'all' }
+                        }
+                        allOf {
+                            /* exclusive filters applied, only run when all filters match */
+                            expression { params.ALIGNMENT_FILTER == env.ALIGNMENT }
+                            expression { params.LOSS_FILTER == env.LOSS }
+                            expression { params.DECODER_FILTER == env.DECODER }
+                        }
+                    }
+                }
+                axes {
+                    axis {
+                        name 'ALIGNMENT'
+                        values 'sparse', 'dense', 'ctcalign'
+                    }
+                    axis {
+                        /*
+                            the biggio et al. objective function doesn't work well for audio so
+                            don't bother with beam search decoder (batch / beam)
+                       */
+                        name 'DECODER'
+                        values 'greedy', 'batch', 'batch_no_lm', 'ds_greedy_no_lm'
+                    }
+                }
+                stages {
+                    stage("Pull docker image") {
+                        steps {
+                                sh "docker pull ${IMAGE}"
+                        }
+                    }
+                    stage("Run experiment") {
+                        when {
+                            expression { params.JOB_TYPE == 'run' }
+                        }
+                        steps {
+                            /* Run the attacks! */
+                            sh  """
+                                docker run \
+                                    --gpus device=\${GPU_N} -t --rm --shm-size=10g --pid=host \
+                                    --name ${DOCKER_NAME} \
+                                    -v ${DOCKER_MOUNT} \
+                                    -e ${DOCKER_UID} \
+                                    -e ${DOCKER_GID} \
+                                    -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
+                                    -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
+                                    ${IMAGE} \
+                                    ${PYTHON_CMD}
+                                """
+                                archiveArtifacts "${BUILD_ID}/**"
+                        }
+                    }
+                    stage("Run test") {
+                        when {
+                            expression { params.JOB_TYPE == 'test' }
+                        }
+                        steps {
+                            sh  """
+                                docker run \
+                                    --gpus device=\${GPU_N} -t --rm --shm-size=10g --pid=host \
+                                    --name ${DOCKER_NAME} \
+                                    ${IMAGE} \
+                                    ${PYTHON_CMD}
+                                """
+                        }
+                    }
+                }
+                post {
+                    always {
+                        sh "docker image prune -f"
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ conf-weightedmaxmin/unbounded.py
@@ -0,0 +1,372 @@
+#!/usr/bin/env python3
+import os
+import traceback
+import multiprocessing as mp
+import tensorflow as tf
+
+from cleverspeech import data
+from cleverspeech import graph
+from cleverspeech.utils.Utils import log
+from cleverspeech.utils.runtime.ExperimentArguments import args
+from cleverspeech.graph.CTCAlignmentSearch import create_tf_ctc_alignment_search_graph
+
+# victim model
+from SecEval import VictimAPI as DeepSpeech
+
+
+class CustomLoss(graph.Losses.BaseLogitDiffLoss):
+    def __init__(self, attack, target_logits, weight_settings=(1.0, 1.0)):
+
+        super().__init__(
+            attack,
+            target_logits,
+            softmax=True,
+            weight_settings=weight_settings,
+        )
+
+        self.c = c = 2 - self.target_logit
+        diff = c * (- self.target_logit + self.max_other_logit)
+
+        self.loss_fn = tf.reduce_sum(diff, axis=1)
+        self.loss_fn += attack.batch.audios["max_feats"]  # loss = 0 when min'd
+        self.loss_fn *= self.weights
+
+
+def local_writer_fn(queue, settings):
+
+    import traceback
+    from cleverspeech.data.egress import transform, load
+
+    # set up the directory we'll use for local results data
+
+    if not os.path.exists(settings["outdir"]):
+        os.makedirs(settings["outdir"], exist_ok=True)
+        log("Created new directory: {}".format(settings["outdir"]))
+
+    # Write the current settings to "settings.json" file.
+
+    settings_db = load.LocalJsonMetadataFile(settings["outdir"])
+    settings_db.open("settings")
+    settings_db.put(settings)
+    log("Wrote settings.")
+
+    json_db = load.LocalJsonMetadataFile(settings["outdir"])
+    wav_db = load.LocalWavFiles(settings["outdir"])
+
+    while True:
+
+        results = queue.get()
+
+        if results == "dead":
+
+            # This is a signal telling us there's no more data inbound.
+            # Obviously we don't want to write this to disk, so tell parent
+            # that we're done with this item and break the while loop.
+
+            queue.task_done()
+            break
+
+        elif data != "dead":
+            try:
+                for example_data in transform.unbounded_transforms(results):
+                    log(
+                        transform.unbounded_logging(example_data),
+                        wrap=False,
+                        outdir=settings["outdir"],
+                        stdout=False,
+                        timings=True,
+                    )
+                    if example_data["success"] is True:
+
+                        db_file_path = example_data['basenames'].rstrip(".wav")
+                        json_db.open(db_file_path)
+                        json_db.put(example_data)
+
+                        # -- Write audio data.
+
+                        for wav_file in ["audio", "deltas", "advs"]:
+
+                            wav_db.open(db_file_path + "_{}".format(wav_file))
+                            wav_db.put(example_data[wav_file])
+
+            except Exception as e:
+                tb = "".join(
+                    traceback.format_exception(None, e, e.__traceback__))
+
+                s = "Something broke during file writes!"
+                s += "\n\nError Traceback:\n{e}".format(e=tb)
+                log(s, wrap=True)
+                raise
+
+            finally:
+                queue.task_done()
+
+
+def s3_writer_fn(queue, settings):
+
+    import os
+    import traceback
+    from cleverspeech.data.egress import transform, load
+
+    # set up the directory we'll use for local logging data
+
+    if not os.path.exists(settings["outdir"]):
+        os.makedirs(settings["outdir"], exist_ok=True)
+        log("Created new directory: {}".format(settings["outdir"]))
+
+    # Write the current settings to "settings.json" file.
+
+    settings_db = load.S3JsonMetadataFile("cleverspeech-results")
+    settings_db.open(os.path.join(settings["outdir"], "settings"))
+    settings_db.put(settings)
+    log("Wrote settings.")
+
+    metadata_db = load.S3JsonMetadataFile("cleverspeech-results")
+
+    while True:
+
+        results = queue.get()
+
+        if results == "dead":
+
+            # This is a signal telling us there's no more data inbound.
+            # Obviously we don't want to write this to disk, so tell parent
+            # that we're done with this item and break the while loop.
+
+            queue.task_done()
+            break
+
+        elif data != "dead":
+            try:
+                for example_data in transform.unbounded_transforms(results):
+                    log(
+                        transform.unbounded_logging(example_data),
+                        wrap=False,
+                        outdir=settings["outdir"],
+                        stdout=False,
+                        timings=True,
+                    )
+                    if example_data["success"] is True:
+
+                        file_path = example_data['basenames'].rstrip(".wav")
+
+                        # S3 writes write out the metadata for *all* examples
+                        # not just the latest one
+
+                        file_path = os.path.join(
+                            file_path,
+                            "step_{}".format(example_data["step"])
+                        )
+
+                        metadata_db.open(
+                            os.path.join(settings["outdir"], file_path)
+                        )
+                        metadata_db.put(example_data)
+
+            except Exception as e:
+                tb = "".join(
+                    traceback.format_exception(None, e, e.__traceback__))
+
+                s = "Something broke during file writes!"
+                s += "\n\nError Traceback:\n{e}".format(e=tb)
+                log(s, wrap=True)
+                raise
+
+            finally:
+                queue.task_done()
+
+
+def executor(results_queue, settings, batch, attack_fn):
+    from cleverspeech.data.egress import extract
+    from cleverspeech.utils.runtime.TensorflowRuntime import TFRuntime
+    from cleverspeech.utils.Utils import log
+
+    # tensorflow sessions can't be passed between processes
+    tf_runtime = TFRuntime(settings["gpu_device"])
+
+    with tf_runtime.session as sess, tf_runtime.device as tf_device:
+
+        # Initialise attack graph constructor function
+        attack = attack_fn(sess, batch, settings)
+
+        # log some useful things for debugging before the attack runs
+        attack.validate()
+
+        s = "Created Attack Graph and Feeds. Loaded TF Operations:"
+        log(s, wrap=False)
+        log(funcs=tf_runtime.log_attack_tensors)
+
+        s = "Beginning attack run...\nMonitor progress in: {}".format(
+            settings["outdir"] + "log.txt"
+        )
+        log(s)
+        for is_results_step in attack.run():
+            if is_results_step:
+                res = extract.get_unbounded_attack_state(attack)
+                results_queue.put(res)
+
+
+def manager(settings, attack_fn, batch_gen):
+
+    results_queue = mp.JoinableQueue()
+
+    if settings["writer"] == "local":
+        writer_process = mp.Process(
+            target=local_writer_fn,
+            args=(results_queue, settings)
+        )
+    elif settings["writer"] == "s3":
+        writer_process = mp.Process(
+            target=s3_writer_fn,
+            args=(results_queue, settings)
+        )
+    else:
+        raise NotImplementedError
+
+    writer_process.start()
+    log("Started a writer subprocess.")
+
+    for b_id, batch in batch_gen:
+
+        # we *must* call the tensorflow session within the batch loop so the
+        # graph gets reset: the maximum example length in a batch affects the
+        # size of most graph elements.
+
+        log("Running for Batch Number: {}".format(b_id), wrap=True)
+
+        attack_process = mp.Process(
+            target=executor,
+            args=(results_queue, settings, batch, attack_fn)
+        )
+
+        try:
+            attack_process.start()
+            attack_process.join()
+            attack_process.terminate()
+
+        except Exception as e:
+
+            tb = "".join(traceback.format_exception(None, e, e.__traceback__))
+
+            s = "Something broke! Attack failed to run for these examples:\n"
+            s += '\n'.join(batch.audios["basenames"])
+            s += "\n\nError Traceback:\n{e}".format(e=tb)
+
+            log(s, wrap=True)
+            log("Attempting to close writer queue and subprocess.", wrap=True)
+
+            results_queue.put("dead")
+            results_queue.close()
+            log("Results queue closed.", wrap=True)
+
+            writer_process.join()
+            writer_process.terminate()
+            log("Writer subprocess closed.", wrap=True)
+            raise
+
+    log("Attempting to close writer queue and subprocess.", wrap=True)
+    results_queue.put("dead")
+    results_queue.close()
+    log("Results queue closed.", wrap=True)
+
+    writer_process.join()
+    writer_process.terminate()
+    log("Writer subprocess closed.", wrap=True)
+
+
+# ==============================================================================
+
+
+ALIGNMENT_CHOICES = {
+    "sparse": data.ingress.etl.batch_generators.sparse,
+    "ctcalign": data.ingress.etl.batch_generators.standard,
+    "dense": data.ingress.etl.batch_generators.dense,
+}
+
+
+def create_attack_graph(sess, batch, settings):
+
+    feeds = data.ingress.Feeds.Attack(batch)
+
+    attack = graph.AttackConstructors.UnboundedAttackConstructor(sess, batch, feeds)
+    attack.add_placeholders(graph.Placeholders.Placeholders)
+    attack.add_perturbation_subgraph(
+        graph.PerturbationSubGraphs.Independent
+    )
+    attack.add_victim(
+        DeepSpeech.Model,
+        decoder=settings["decoder"],
+        beam_width=settings["beam_width"]
+    )
+
+    if settings["align"] == "ctcalign":
+
+        alignment = create_tf_ctc_alignment_search_graph(sess, batch)
+
+        attack.add_loss(
+            CustomLoss,
+            alignment.graph.target_alignments,
+        )
+        attack.create_loss_fn()
+        attack.add_optimiser(
+            graph.Optimisers.AdamIndependentOptimiser,
+            learning_rate=settings["learning_rate"]
+        )
+        attack.add_procedure(
+            graph.Procedures.CTCAlignHardcoreMode,
+            alignment_graph=alignment,
+            steps=settings["nsteps"],
+            update_step=settings["decode_step"]
+        )
+
+    else:
+        attack.add_loss(
+            CustomLoss,
+            attack.placeholders.targets,
+        )
+        attack.create_loss_fn()
+        attack.add_optimiser(
+            graph.Optimisers.AdamIndependentOptimiser,
+            learning_rate=settings["learning_rate"]
+        )
+        attack.add_procedure(
+            graph.Procedures.Unbounded,
+            steps=settings["nsteps"],
+            update_step=settings["decode_step"]
+        )
+
+    return attack
+
+
+def attack_run(master_settings):
+    """
+    """
+
+    align = master_settings["align"]
+    decoder = master_settings["decoder"]
+    outdir = master_settings["outdir"]
+
+    outdir = os.path.join(outdir, "unbounded/confidence/weightedmaxmin/")
+    outdir = os.path.join(outdir, "{}/".format(align))
+    outdir = os.path.join(outdir, "{}/".format(decoder))
+
+    master_settings["outdir"] = outdir
+
+    batch_gen = ALIGNMENT_CHOICES[align](master_settings)
+
+    manager(master_settings, create_attack_graph, batch_gen,)
+    log("Finished run.")
+
+
+if __name__ == '__main__':
+
+    log("", wrap=True)
+
+    extra_args = {
+        'align': [str, "sparse", False, ALIGNMENT_CHOICES.keys()],
+    }
+
+    args(attack_run, additional_args=extra_args)
+
+
+
